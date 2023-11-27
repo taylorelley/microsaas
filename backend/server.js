08f40676b601b818e3cf9d2e9c575c24a120b284
taylorelley <48809387+taylorelley@users.noreply.github.com>
2023-11-28 09:56:12 +1300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
 
+app.get('/api/generateGearList', (req, res) => {
+  // Call ML model with trip data
+  const gearList = []; 
+  
+  res.json(gearList);
+});
+
+app.listen(3000, () => {
+  console.log('Server running'); 
+});

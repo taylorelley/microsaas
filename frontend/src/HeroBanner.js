ec1efc295b69d83998b751aff531a5930ccc9a37
taylorelley <48809387+taylorelley@users.noreply.github.com>
2023-11-28 09:53:02 +1300

    Create HeroBanner.js

---
+++ frontend/src/HeroBanner.js
@@ -0,0 +1,11 @@
+export default function HeroBanner() {
+  return (
+    <section className="banner">
+      {/* Illustration asset */}
+
+      <h1>Get personalized packing lists for your next adventure!</h1>
+      
+      <button>Start Now</button>
+    </section>
+  ); 
+}

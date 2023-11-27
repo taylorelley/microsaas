c058143aa364dbf470b1d14126e0eb20761a71c5
taylorelley <48809387+taylorelley@users.noreply.github.com>
2023-11-28 09:55:29 +1300

    Update serviceWorker.js

--- frontend/src/serviceWorker.js
+++ frontend/src/serviceWorker.js
@@ -1,1 +1,9 @@
+const cacheName = 'gear-checklist-v1';
 
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );  
+});
